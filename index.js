cc37f89dab24e67f5b3604eb0d50bc56ecb6caca
levy <lnjiru43@gmail.com>
2022-11-29 01:57:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const divide =(num1,num2) =>2000/100;
+const divide = function() {
+    return 2000/100;
+}
 
-const square =num=>num*num;
 
-const add =(num1,num2)=>num1+num2;
+const square = num => num**2;
+
+const add = (x,y) => x + y;
